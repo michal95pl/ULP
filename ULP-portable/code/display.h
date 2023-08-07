6584c8e789763e81ee32bae546d2ef6d3e4b4536
Michał Lichtarski <85219287+michal95pl@users.noreply.github.com>
2023-08-07 23:18:39 +0200

    add display clear

--- ULP-portable/code/display.h
+++ ULP-portable/code/display.h
@@ -4,5 +4,6 @@
   void display_draw_pixel (CRGB displayBuffer[], int x, int y, CRGB color);
   void display_startup_effect (CRGB displayBuffer[], uint8_t brightness);
   void encode_data(CRGB displayBuffer[], uint16_t data[], uint16_t &length, uint8_t brightness);
-
+  void display_clear(CRGB displayBuffer[]);
+  
 #endif
