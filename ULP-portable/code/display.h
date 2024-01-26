cbcd45f57aa45d2687778b04f619d1a121b40746
Michał Lichtarski <85219287+michal95pl@users.noreply.github.com>
2024-01-26 01:00:59 +0100

    Delete ULP-portable/code directory

--- ULP-portable/code/display.h
+++
@@ -1,9 +0,0 @@
-#ifndef display_lib
-  #define display_lib
-
-  void display_draw_pixel (CRGB displayBuffer[], int x, int y, CRGB color);
-  void display_startup_effect (CRGB displayBuffer[], uint8_t brightness);
-  void encode_data(CRGB displayBuffer[], uint16_t data[], uint16_t &length, uint8_t brightness);
-  void display_clear(CRGB displayBuffer[]);
-  
-#endif
